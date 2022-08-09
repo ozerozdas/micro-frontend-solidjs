900ed5fe6f5548eeb555038787147ec21e007cd1
ozerozdas <96Ozer*?96>
2022-08-09 13:48:52 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Micro Frontend with SolidJS
+
+This project is an example for micro frontend with solidjs. "Host Project" retrieves a Counter from "Remote Project". 
+
+You need to run code in the below for both folders.
+
+    yarn && yarn start
---
+++ remote/src/Counter.jsx
@@ -0,0 +1,14 @@
+import { createSignal } from "solid-js";
+
+export default () => {
+    const [count, setCount] = createSignal(0);
+
+    return (
+        <div>
+            <div>Count = {count()}</div>
+            <button onClick={() => setCount(count() + 1)}>
+                Add One
+            </button>
+        </div>
+    )
+}
